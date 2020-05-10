6ff2a40e4c1b2e386d77e448e114cd3e891ec609
Hugues Tavenard <htavenard@octo.com>
2020-05-10 18:41:24 +0200

    move git_prompt_info to prompt file

--- prompt_theme.sh
+++ prompt_theme.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function git_prompt_info() {
+  local ref
+  ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+}
+
 # Apply theming defaults
 PS1="%n@%m:%~%# "
 
