7eefc29ab6df8e585e486ff21a94a94e236ff6a7
Hugues Tavenard <htavenard@octo.com>
2020-05-12 12:07:39 +0200

    refactor docker compose commands

--- aliases/erpc.sh
+++ aliases/erpc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 crenodispo_dir="$HOME/projects/creno-dispo"
-api_dir="$crenodispo_dir/api"
 
-alias docker-up="docker-compose -f $crenodispo_dir/dev/docker-compose.dev.yml up"
-alias docker-down="docker-compose -f $crenodispo_dir/dev/docker-compose.dev.yml down"
+docker_compose_path="$crenodispo_dir/dev/docker-compose.dev.yml"
+alias docker-up="docker-compose -f $docker_compose_path up"
+alias docker-down="docker-compose -f $docker_compose_path down"
 alias run-docker-compose="docker-down && docker-up --build"
 
 alias fake-smtp="java -jar $crenodispo_dir/fakeSMTP-2.0.jar  -o ~/Desktop/creno_dispo/smtp_output"
 
-# gatling
+
 alias gatling-editeur-prise-sous-mandat="gradle clean gatlingRun-fr.gouv.interieur.formation.priseSousMandat.PriseSousMandatEditeurSimulation"
 alias gatling-front-prise-sous-mandat="gradle clean gatlingRun-fr.gouv.interieur.formation.priseSousMandat.PriseSousMandatFrontSimulation"
 
@@ -22,6 +22,7 @@ alias run-batch-reprise="build-batch-reprise && \
             default"
 
 
+api_dir="$crenodispo_dir/api"
 alias run-tests="$api_dir/gradlew -p $api_dir test \
     --tests \"fr.gouv.interieur.creno_dispo.domain.*\" \
     --tests \"fr.gouv.interieur.creno_dispo.application.rest.dsr.candidat.*\" \
