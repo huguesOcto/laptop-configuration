4f1ffeaa2d8f72c2d9937af7c7c91143866d5937
Hugues Tavenard <htavenard@octo.com>
2020-05-25 09:11:21 +0200

    update erpc api test command

--- aliases/erpc.sh
+++ aliases/erpc.sh
@@ -25,7 +25,8 @@ alias run-batch-reprise="build-batch-reprise && \
 api_dir="$crenodispo_dir/api"
 alias run-tests="$api_dir/gradlew -p $api_dir test \
     --tests \"fr.gouv.interieur.creno_dispo.domain.*\" \
-    --tests \"fr.gouv.interieur.creno_dispo.application.rest.dsr.candidat.*\" \
+    --tests \"fr.gouv.interieur.creno_dispo.use_cases.*\" \
     --tests \"fr.gouv.interieur.creno_dispo.application.rest.dsr.examen.*\" \
-    --tests \"fr.gouv.interieur.creno_dispo.infrastructure.database.candidat*\""
+    --tests \"fr.gouv.interieur.creno_dispo.infrastructure.database.creneau*\" \
+    --tests \"fr.gouv.interieur.creno_dispo.infrastructure.database.examen*\""
 
