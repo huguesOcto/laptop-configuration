247f5ea6804c9bd056bd059099354d7cb34209c0
Hugues Tavenard <htavenard@octo.com>
2020-08-07 11:17:07 +0200

    add crenodispo aliases

--- aliases/erpc.sh
+++ aliases/erpc.sh
@@ -3,25 +3,17 @@
 export crenodispo_dir="$HOME/projects/creno-dispo"
 export docker_compose_path="$crenodispo_dir/dev/docker-compose.dev.yml"
 
+# crenodispo-cli
 alias cre="crenodispo"
+alias run-docker-compose="crenodispo restart-docker-compose"
+alias run-batch-reprise="crenodispo run-batch-reprise"
+alias mrc="crenodispo gitlab-mr --current"
+alias mrs="crenodispo gitlab-mr --search"
 
-alias docker-up="docker-compose -f $docker_compose_path up"
-alias docker-down="docker-compose -f $docker_compose_path down"
-alias run-docker-compose="docker-down && docker-up --build"
 
 alias fake-smtp="java -jar $crenodispo_dir/fakeSMTP-2.0.jar  -o ~/Desktop/creno_dispo/smtp_output"
-
-
 alias gatling-editeur-prise-sous-mandat="gradle clean gatlingRun-fr.gouv.interieur.formation.priseSousMandat.PriseSousMandatEditeurSimulation"
 alias gatling-front-prise-sous-mandat="gradle clean gatlingRun-fr.gouv.interieur.formation.priseSousMandat.PriseSousMandatFrontSimulation"
-
-
-alias build-batch-reprise="docker build -t batch-reprise $crenodispo_dir/batch_reprise"
-alias run-batch-reprise="build-batch-reprise && \
-            docker run --rm --network=dev_default \
-            --env-file $crenodispo_dir/batch_reprise/env/datasource.env \
-            batch-reprise \
-            default"
 
 
 api_dir="$crenodispo_dir/api"
