59c98e3537ac909825c5a76528dd63934409299f
Hugues Tavenard <htavenard@octo.com>
2020-09-28 09:24:11 +0200

    update daily link

--- cool_oh_my_zsh_functions.sh
+++ cool_oh_my_zsh_functions.sh
@@ -41,7 +41,7 @@ function c() {
 }
 
 function m() {
-  local lien_daily='https://meet.google.com/vbu-esjw-jxx'
+  local lien_daily='https://meet.google.com/cgj-uzcb-ghc'
   local lien_perso='https://meet.google.com/jbt-nusx-bjm'
   local lien=""
 
